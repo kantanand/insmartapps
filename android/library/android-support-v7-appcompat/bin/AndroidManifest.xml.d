59317c2f767aab2bafc5cd06c0ec081a8290a88b
Kantanand <kantanand.usk@gmail.com>
2016-04-17 18:08:40 +0530

    removed unwanted fiels

--- android/library/android-support-v7-appcompat/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/kantanand/projects/insmartworld/android/library/android-support-v7-appcompat/bin/AndroidManifest.xml : \
-/home/kantanand/projects/insmartworld/android/library/android-support-v7-appcompat/AndroidManifest.xml \
