76d2af730e31791fa355f0bf46463e08677ff49b
Kantanand <kantanand.usk@gmail.com>
2016-05-08 03:26:01 +0530

    learning index view and urls file added

--- server/learning/signup/views.py
+++ server/learning/signup/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    return HttpResponse('signup page ')
